3766d724b3099c641c90e5206e95df4a6973b363
theodaguier <92515854+theodaguier@users.noreply.github.com>
2022-09-13 15:55:52 +0200

    Initial Commit

---
+++ First-IOS-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  First-IOS-App
+//
+//  Created by Théo Daguier on 13/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ First-IOS-App/First_IOS_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  First_IOS_AppApp.swift
+//  First-IOS-App
+//
+//  Created by Théo Daguier on 13/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct First_IOS_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
